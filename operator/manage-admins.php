5e1f7a14bb56f34e548ec08fd287105cb9433c97
= <travis@travis-ci.org>
2016-01-21 16:08:44 +0100

    "Session bugs fixed"

--- operator/manage-admins.php
+++ operator/manage-admins.php
@@ -1,5 +1,5 @@
-﻿<?php 
-session_start(); 
+<?php session_start(); ?>
+<?php
 define("CONST_FILE_PATH", "../includes/constants.php");
 include ('../classes/WebPage.php'); //Set up page as a web page
 $thisPage = new WebPage(); //Create new instance of webPage class
